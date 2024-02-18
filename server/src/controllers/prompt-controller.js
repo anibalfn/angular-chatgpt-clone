b9e0dbcfbc7e5f123cddca377481e34e2af1f53e
anibalfn <anibalneto29@gmail.com>
2024-02-18 07:59:56 -0300

    Add asyncronism try/catch

--- server/src/controllers/prompt-controller.js
+++ server/src/controllers/prompt-controller.js
@@ -9,7 +9,17 @@ module.exports = {
             const response = await openaiAPI.createCompletion(
                 openai.textComplete('Cite 5 animais')
             )
-        }
+        
+                return res.status(200).json({
+                    success: true,
+                    data: response.data.choices[0].text,
+                })
+        
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                error: error.response ? error.response : 'Something went wrong',
+        })
     }
 
-}
+}}
