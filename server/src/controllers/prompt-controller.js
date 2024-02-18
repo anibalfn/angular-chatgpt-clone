816f990d6d5c534374d3eb4a71c05dccaf253364
anibalfn <anibalneto29@gmail.com>
2024-02-18 08:07:12 -0300

    Add routes.js

--- server/src/controllers/prompt-controller.js
+++ server/src/controllers/prompt-controller.js
@@ -1,13 +1,15 @@
+const InputPrompt = require('../models/input-prompt');
 const openai = require('../config/openai');
 
 module.exports = {
     async sendText(req, res) {
         const openaiAPI = openai.configuration()
         // retorna instância nova
+        const inputModel = new InputPrompt (req.body)
 
         try {
             const response = await openaiAPI.createCompletion(
-                openai.textComplete('Cite 5 animais')
+                openai.textComplete(inputModel)
             )
         
                 return res.status(200).json({
